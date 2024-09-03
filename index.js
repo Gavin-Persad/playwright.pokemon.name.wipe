139add63ff92206c5788f8be52f5172ac0ae7fd7
Gavin-Persad <gavinapersad@gmail.com>
2024-09-03 13:59:35 +0100

    basic program added to index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+import { chromium } from 'playwright';
+import randomUseragent from 'random-useragent';
+import fs from 'fs/promises';
+
+async function main() {
+	// Create Random Agent
+	const randomAgent = randomUseragent.getRandom();
+
+	// Launch browser
+	const browser = await chromium.launch({ headless: true });
+	const context = await browser.newContext({ userAgent: randomAgent });
+	const page = await context.newPage({ byPassCSP: true });
+	try {
+		await page.goto('https://pokemondb.net/pokedex/all');
+		page.setDefaultTimeout(10000);
+		await page.setViewportSize({ width: 800, height: 600 });
+
+		// Add a delay (optional) to ensure the page fully loads
+		await page.waitForTimeout(2000);
+
+		const screenshot = await page.screenshot({
+			path: 'pokemon-screenshot.png',
+		});
+		console.log('Screenshot saved as pokemon-screenshot.png');
+
+		await browser.close();
+	} catch (error) {
+		console.error('An error occurred:', error);
+	}
+}
+
+main();
